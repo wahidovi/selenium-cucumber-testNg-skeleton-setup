2977cddc86b7f7e39fe07034ff6614988d1f85e6
wahidovi <md.wahidovi@gmail.com>
2022-04-23 23:28:46 -0700

    added html reports

--- src/test/java/testRunner/FBtestRunner.java
+++ src/test/java/testRunner/FBtestRunner.java
@@ -3,11 +3,14 @@ package testRunner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(tags = "", 
-				features = {"Features/"}, 
-				glue = {"stepDefs"}
-				
-		)
+@CucumberOptions(tags = "", features = { "Features/" }, 
+				glue = { "stepDefs" }, 
+				plugin = { "pretty",
+						"html:target/cucumber-html-reports.html"
+
+						}
+
+)
 
 public class FBtestRunner extends AbstractTestNGCucumberTests {
 
